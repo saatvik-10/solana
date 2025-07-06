4b8674a62f4ea85149019a091b4b2a0d718aeb4c
saatvik-10 <saatvikmadan529@gmail.com>
2025-07-06 19:24:22 +0530

    added airdrop

---
+++ airdrop/index.ts
@@ -0,0 +1,16 @@
+import { PublicKey, Connection, LAMPORTS_PER_SOL } from '@solana/web3.js';
+
+export const airdrop = async (address: string, amount: number) => {
+  const publicKey = new PublicKey(address);
+  const conn = new Connection('http://localhost:8899', 'confirmed');
+
+  const signature = await conn.requestAirdrop(
+    publicKey,
+    amount * LAMPORTS_PER_SOL
+  );
+  await conn.confirmTransaction(signature);
+};
+
+airdrop('3sXPJophwvX6crRSEoKXXdH6hLRdNKKUazwaXEpuxhLS', 1);
+
+// 10^9 LAMPORTS = 1 SOL
